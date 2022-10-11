7fd836e6b3d4cae888f9b19b860624e92cc614bf
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-10-11 14:38:22 -0400

    added components for Auth0

--- pokedex/App.js
+++ pokedex/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import axios from 'axios';
 import { render } from '@testing-library/react';
+import { withAuth0 } from '@auth0/auth0-react';
 
 const API_SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
     constructor(props) {
+        super(props);
         this.state = {
           pokedex: []
         }
     }
+
+
+  async componentDidMount() {
+    if (this.props.auth0.isAuthenticated) {
+      const res = await this.props.auth0.getIdTokenClaims();
+      const jwt = res.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${jwt}` },
+        method: 'get',
+        baseURL: process.env.REACT_APP_SERVER,
+        url: '/pokedex'
+      }
+    }
+  }
 }
 
-getPokemons = async () => {
-  const response = await axios.get(`${API_SERVER}/pokedex,`);
-  this.setState({})
-}
+
+
+// getPokemons = async () => {
+//   const response = await axios.get(`${API_SERVER}/pokedex,`);
+//   this.setState({})
+// }
 
 
 render() {
@@ -27,4 +45,4 @@ render() {
 }
 
 
-export default App;
+export default withAuth0(App);
